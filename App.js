c7617fbc542be4619fcb541f1c50f7fd090aa3b6
do tien anh <anhdt.ute@gmail.com>
2021-05-10 18:25:32 +0700

    flex-2 react native

--- App.js
+++ App.js
@@ -1,185 +1,109 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {
-    Text,
-    SafeAreaView,
-    StyleSheet,
-    View,
-    Button,
-    TouchableOpacity,
-} from 'react-native';
+import {Text, SafeAreaView, View} from 'react-native';
 //import DefaultView from './src/DefaultView';
 export default class App extends Component {
     render() {
         return (
             <SafeAreaView style={{flex: 1, flexDirection: 'column'}}>
-                <View style={{flex: 0.4, flexDirection: 'column'}}>
-                    <View
+                <View
+                    style={{
+                        flex: 0.3,
+                        backgroundColor: 'red',
+                        borderWidth: 1.5,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                    <Text
                         style={{
-                            flex: 1,
-                            backgroundColor: 'cyan',
-                            justifyContent: 'center',
-                            alignItems: 'center',
+                            color: 'white',
+                            fontSize: 20,
+                            fontWeight: 'bold',
                         }}>
-                        <Text style={{textColor: 'white'}}>1</Text>
-                    </View>
-                    <View
-                        style={{
-                            flex: 1,
-                            backgroundColor: 'pink',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}>
-                        <Text style={{textColor: 'white'}}>2</Text>
-                    </View>
-                    <View
-                        style={{
-                            flex: 1,
-                            backgroundColor: 'blue',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}>
-                        <Text style={{textColor: 'white'}}>3</Text>
-                    </View>
-                    <View
-                        style={{
-                            flex: 1,
-                            backgroundColor: 'red',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}>
-                        <Text style={{textColor: 'white'}}>4</Text>
-                    </View>
+                        1
+                    </Text>
                 </View>
-                <View style={{flex: 0.6, flexDirection: 'row'}}>
+
+                <View style={{flex: 0.7, flexDirection: 'row'}}>
                     <View
                         style={{
-                            flex: 1,
-                            backgroundColor: 'khaki',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}>
-                        <Text style={{textColor: 'white'}}>1</Text>
-                    </View>
-                    <View
-                        style={{
-                            flex: 1,
-                            backgroundColor: 'gray',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}>
-                        <Text style={{textColor: 'white'}}>2</Text>
-                    </View>
-                    <View
-                        style={{
-                            flex: 1,
-                            backgroundColor: 'blueviolet',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}>
-                        <Text style={{textColor: 'white'}}>3</Text>
-                    </View>
-                    <View
-                        style={{
-                            flex: 1,
+                            flex: 0.3,
                             backgroundColor: 'pink',
+                            borderRightWidth: 1.5,
+                            borderBottomWidth: 1.5,
+                            borderLeftWidth: 1.5,
                             justifyContent: 'center',
                             alignItems: 'center',
                         }}>
-                        <Text style={{textColor: 'white'}}>4</Text>
+                        <Text
+                            style={{
+                                color: 'white',
+                                fontSize: 20,
+                                fontWeight: 'bold',
+                            }}>
+                            2
+                        </Text>
+                    </View>
+                    <View style={{flex: 0.7, flexDirection: 'column'}}>
+                        <View
+                            style={{
+                                flex: 0.7,
+                                backgroundColor: 'blue',
+                                borderRightWidth: 1.5,
+                                borderBottomWidth: 1.5,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}>
+                            <Text
+                                style={{
+                                    color: 'white',
+                                    fontSize: 20,
+                                    fontWeight: 'bold',
+                                }}>
+                                3
+                            </Text>
+                        </View>
+                        <View style={{flex: 0.3, flexDirection: 'row'}}>
+                        <View
+                            style={{
+                                flex: 0.5,
+                                backgroundColor: 'brown',
+                                borderRightWidth: 1.5,
+                                borderBottomWidth: 1.5,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}>
+                            <Text
+                                style={{
+                                    color: 'white',
+                                    fontSize: 20,
+                                    fontWeight: 'bold',
+                                }}>
+                                4
+                            </Text>
+                        </View>
+                        <View
+                            style={{
+                                flex: 0.5,
+                                backgroundColor: 'green',
+                                borderRightWidth: 1.5,
+                                borderBottomWidth: 1.5,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}>
+                            <Text
+                                style={{
+                                    color: 'white',
+                                    fontSize: 20,
+                                    fontWeight: 'bold',
+                                }}>
+                                5
+                            </Text>
+                        </View>
+                        </View>
                     </View>
                 </View>
             </SafeAreaView>
         );
     }
 }
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         flexDirection: 'column',
-//     },
-//     containerText: {
-//         flex: 0.4,
-//         flexDirection: 'column',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//     },
-//     containerText2: {
-//         flex: 0.6,
-//         flexDirection: 'row',
-//         justifyContent: 'center',
-//         textAlign: 'center',
-//         // alignItems: 'stretch',
-//     },
-// });
-//             <SafeAreaView style={styles.container}>
-//                 <View style={styles.containerText}>
-//                     <Default
-//                         justifyContent: 'center',
-//                         title="view100"
-//                         color="red"
-//                         textColor="white"
-//                     />
-//                     <DefaultView title="view200" color="cyan" />
-//                     <DefaultView title="view300" color="green" />
-//                     <DefaultView title="view400" color="pink" />
-//                 </View>
-//                 <View style={styles.containerText2}>
-//                     <DefaultView title="view100" color="red" />
-//                     <DefaultView title="view200" color="cyan" />
-//                     <DefaultView title="view300" color="green" />
-//                     <DefaultView title="view400" color="pink" />
-//                 </View>
-//             </SafeAreaView>
-//         );
-//     }
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         flexDirection: 'column',
-//     },
-//     containerText: {
-//         flex: 0.4,
-//         flexDirection: 'column',
-//         // justifyContent: 'center',
-//         // alignItems: 'center',
-//     },
-//     containerText2: {
-//         flex: 0.6,
-//         flexDirection: 'row',
-//     },
-//     containerTouchable: {
-//         marginTop: 20,
-//         flexDirection: 'row',
-//         justifyContent: 'space-evenly',
-//     },
-//     textStyleEn: {
-//         color: 'green',
-//         fontWeight: 'bold',
-//     },
-//     textStyleVn: {
-//         fontSize: 20,
-//         color: 'red',
-//         fontWeight: 'bold',
-//     },
-//     touchForgot: {
-//         padding: 10,
-//         backgroundColor: 'green',
-//         borderRadius: 5,
-//     },
-//     touchRemove: {
-//         padding: 10,
-//         backgroundColor: 'yellow',
-//         borderRadius: 5,
-//     },
-//     textTouchForgot: {
-//         fontSize: 14,
-//         color: 'white',
-//     },
-//     textTouchRemove: {
-//         fontSize: 14,
-//         color: 'black',
-//     },
-// });

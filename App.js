2476d3cb685ab7ec1b3f5acdc812dce3b1fad4e8
do tien anh <anhdt.ute@gmail.com>
2021-05-11 11:58:53 +0700

    rn - xử lí dữ liệu

--- App.js
+++ App.js
@@ -1,34 +1,45 @@
-/* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {Text, SafeAreaView, View, Platform} from 'react-native';
-import DefaultView from './src/DefaultView';
+import {Text, SafeAreaView, View, StyleSheet} from 'react-native';
+// import DefaultView from './src/DefaultView';
 export default class App extends Component {
+    shouldRenderVn = (word) => {
+        if (word.isMemorized) {
+            return '----';
+        } else {
+            return word.vn;
+        }
+    };
     render() {
+        const word = {en: 'One', vn: 'Một', isMemorized: true};
         return (
-            <SafeAreaView style={{flex: 1, flexDirection: 'column'}}>
-                <View style={{flex: 0.5, flexDirection: 'row'}}>
-                    <View style={{flex: 0.2}}>
-                    <DefaultView title="1" color="green" textColor="white"/></View>       
-                    <View style={{flex: 0.8, flexDirection: 'column'}}>
-                        <DefaultView title="3" color="red" textColor="white"/>
-                        <DefaultView title="4" color="pink" textColor="white"/>
-                    </View> 
-                </View>
-
-                <View style={{flex: 0.5, flexDirection: 'row'}}>
-                    <View style={{flex: 0.2}}>
-                    <DefaultView title="2" color="red" textColor="white"/></View>
-                    <View style={{flex: 0.5, flexDirection: 'column'}}> 
-                        <DefaultView title="5" color="cyan" textColor="white"/>
-                        <DefaultView title="6" color="violet" textColor="white"/>
-                    </View>
-                    <View style={{flex: 0.3, flexDirection: 'column'}}>
-                        <DefaultView title="7" color="brown" textColor="white"/>
-                        <DefaultView title="8" color="orange" textColor="white"/>
-                        <DefaultView title="9" color="black" textColor="white"/>
-                    </View>
-                </View>
+            <SafeAreaView style={styles.container}>
+                <Text style={styles.textStyleEn}>{word.en}</Text>
+                <Text style={styles.textStyleVn}>
+                    {/* cách 1 */}
+                    {/* {word.isMemorized ? '---' : word.vn} */}
+                    
+                    {/* cách 2, dùng khi render nhiều: tạo chương trình con */}
+                    {this.shouldRenderVn(word)}
+                </Text>
             </SafeAreaView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    },
+    textStyleEn: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'green',
+    },
+    textStyleVn: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'red',
+    },
+});

6568337d380c93cd6af59305511c324a4a9645ee
do tien anh <anhdt.ute@gmail.com>
2021-05-12 17:14:58 +0700

    tách component

--- App.js
+++ App.js
@@ -2,60 +2,106 @@
 import React, {Component} from 'react';
 import {
     Text,
-    View,
     SafeAreaView,
     StyleSheet,
+    View,
     TouchableOpacity,
 } from 'react-native';
 
-export default class App extends Component {
+export default class Main extends Component {
+    renderWord = (word) => {
+        return (
+            <View style={styles.containerWord}>
+                <View style={styles.containerText}>
+                    <Text style={styles.textStyleEn}>{word.en}</Text>
+                    <Text style={styles.textStyleVn}>
+                        {word.isMemorized ? '----' : word.vn}
+                    </Text>
+                </View>
+                <View style={styles.containerTouchable}>
+                    <TouchableOpacity
+                        style={{
+                            ...styles.touchForgot,
+                            backgroundColor: word.isMemorized ? 'green' : 'red',
+                        }}>
+                        <Text style={styles.textTouchForgot}>
+                            {word.isMemorized ? 'Forgot' : 'Memorized'}
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.touchRemove}>
+                        <Text style={styles.textTouchRemove}>Remove</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    };
     render() {
-        const word = {en: 'One', vn: 'Một', isMemorized: false};
+        const words = [
+            {id: 1, en: 'One', vn: 'Một', isMemorized: false},
+            {id: 2, en: 'Two', vn: 'Hai', isMemorized: false},
+            {id: 3, en: 'Three', vn: 'Ba', isMemorized: true},
+            {id: 4, en: 'Four', vn: 'Bốn', isMemorized: true},
+            {id: 5, en: 'Five', vn: 'Năm', isMemorized: false},
+        ];
         return (
             <SafeAreaView style={styles.container}>
-                <View style={styles.containerWord}>
-                    <View style={styles.containerText}>
-                        <Text style={styles.textStyleEn}>{word.en}</Text>
-                        <Text style={styles.textStyleVn}>
-                            {word.isMemorized ? '----' : word.vn}
-                        </Text>
-                    </View>
-                    <View style={styles.containerTouchable}>
-                        <TouchableOpacity
-                            style={{
-                                ...styles.touchForgot, // ... sao chép thuộc tính trong touchForgot vòo mảng hiện tại
-                                backgroundColor: word.isMemorized
-                                    ? 'green'
-                                    : 'red',
-                            }}>
-                            <Text style={styles.textTouchForgot}>
-                                {word.isMemorized ? 'Forgot' : 'Memorized'}
-                            </Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.touchRemove}>
-                            <Text style={styles.textTouchRemove}>Remove</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
+                {words.map((word) => this.renderWord(word))}
             </SafeAreaView>
         );
     }
 }
+// ismemorized : Forgot - màu xanh
+// isMemorized == false : Memorized - màu đỏ
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginHorizontal: 20, // cách đều 2 bên
+        marginHorizontal: 20,
     },
     containerWord: {
+        marginTop: '2%',
         justifyContent: 'center',
-        height: '13%', // kích thước chuẩn
+        height: '13%',
         flexDirection: 'column',
         backgroundColor: 'gainsboro',
         elevation: 5,
-        borderRadius: 5, // bo tròn
+        borderRadius: 5,
     },
     containerText: {
         flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    },
+    containerTouchable: {
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    },
+    textStyleEn: {
+        fontSize: 20,
+        color: 'green',
+        fontWeight: 'bold',
+    },
+    textStyleVn: {
+        fontSize: 20,
+        color: 'red',
+        fontWeight: 'bold',
+    },
+    touchForgot: {
+        padding: 10,
+        backgroundColor: 'green',
+        borderRadius: 5,
+    },
+    touchRemove: {
+        padding: 10,
+        backgroundColor: 'yellow',
+        borderRadius: 5,
+    },
+    textTouchForgot: {
+        fontSize: 14,
+        color: 'white',
+    },
+    textTouchRemove: {
+        fontSize: 14,
+        color: 'black',
     },
 });
